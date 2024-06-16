2d907b91222a095a47ccc00de6c33520ae047101
chathushkaayash <chathushkaayash@gmail.com>
2024-06-17 00:20:55 +0530

    add infinite scrolling product landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import ProductGrid from "./components/ProductGrid";
+import Landing from "./pages/Landing";
 import Login from "./pages/Login";
+
+export interface ProductQuery {
+  genreId?: number;
+  platformId?: number;
+  sortOrder: string;
+  searchText: string;
+}
 
 function App() {
   return (
     <>
       <Navbar />
       <Login />
+      {/* <Landing/> */}
     </>
   );
 }
