455b411bcf25f7097c9f1d9a56ff52781e224628
BimsaraAnjanaJay <bimsarajayadewa@gmail.com>
2024-06-20 00:40:19 +0530

    Set the colors and react route dom

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Test from "./pages/Test";
+import ConsumerProfile from "./pages/ConsumerProfile";
 
 
 
@@ -30,12 +31,13 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/cart" element={<CartDetails />} />
-        <Route path="/landing" element={<Landing />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/test" element={<Test />} />
+        <Route path="/ConsumerProfile" element={<ConsumerProfile />} />
       </Routes>
       
     </Router>
