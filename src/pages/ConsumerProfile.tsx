3109acffaecdbd17144e1b592d9848fc3af54252
BimsaraAnjanaJay <bimsarajayadewa@gmail.com>
2024-06-20 19:35:56 +0530

    Create Consumer Profile Page

---
+++ src/pages/ConsumerProfile.tsx
@@ -0,0 +1,12 @@
+import MiddleContainer from "../components/Containers/MiddleContainer";
+import ProfileDetail from "../components/ProfileDetail";
+
+const ConsumerProfile = () => {
+  return (
+    <MiddleContainer >
+        <ProfileDetail />
+    </MiddleContainer>
+  );
+}
+
+export default ConsumerProfile;
