ff4285d2c7e6cc79c8b9e1413aed384c98aa2ce2
chathushkaayash <chathushkaayash@gmail.com>
2024-06-21 17:33:54 +0530

    implement section in landing

--- src/components/Landing/BrowseByCategory.tsx
+++ src/components/Landing/BrowseByCategory.tsx
@@ -1,7 +1,23 @@
-import { Box, Center, HStack, IconButton, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Image,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { useRef } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 import SimpleSlider, { SliderMethods } from "../SimpleSlider";
+
+import BeveragesIcon from "../../assets/landing/categoryIcons/beverages.svg";
+import FrozenIcon from "../../assets/landing/categoryIcons/frozen.svg";
+import GroceryIcon from "../../assets/landing/categoryIcons/grocery.svg";
+import Section from "./Section";
 
 const BrowseByCategory = () => {
   const sliderRef = useRef<SliderMethods>(null);
@@ -24,67 +40,86 @@ const BrowseByCategory = () => {
   };
 
   const categories = [
-    "Beverages",
-    "Chilled",
-    "Frozen",
-    "Grocery",
-    "Household",
-    "Personal Care",
-    "Pharmacy",
-    "Snacks",
-    "Others",
+    { name: "Beverages", icon: BeveragesIcon },
+    { name: "Frozen", icon: FrozenIcon },
+    { name: "Grocery", icon: GroceryIcon },
+    { name: "Household", icon: BeveragesIcon },
+    { name: "Personal Care", icon: BeveragesIcon },
+    { name: "Pharmacy", icon: BeveragesIcon },
+    { name: "Snacks", icon: BeveragesIcon },
+    { name: "Others", icon: BeveragesIcon },
   ];
 
+  const rightSide = (
+    <HStack gap={5}>
+      <IconButton
+        aria-label="Next"
+        icon={<FaArrowLeft />}
+        bg="primary"
+        color="white"
+        width="fit-content"
+        onClick={previousSlide}
+      />
+      <IconButton
+        aria-label="Next"
+        icon={<FaArrowRight />}
+        bg="primary"
+        color="white"
+        width="fit-content"
+        onClick={nextSlide}
+      />
+    </HStack>
+  );
+
   return (
-    <Stack w="full" px="14vw" h={500}>
-      <HStack>
-        <Box w={5} h={45} bg="primary" borderRadius="lg"></Box>
-        <Text fontSize="lg" fontWeight={600} color="primary">
-          Category
-        </Text>
-      </HStack>
-      <HStack justifyContent="space-between">
-        <Text fontSize="4xl" fontWeight={600}>
-          Browse By Category
-        </Text>
-        <HStack gap={5}>
-          <IconButton
-            aria-label="Next"
-            icon={<FaArrowLeft />}
-            bg="primary"
-            color="white"
-            width="fit-content"
-            onClick={previousSlide}
-          />
-          <IconButton
-            aria-label="Next"
-            icon={<FaArrowRight />}
-            bg="primary"
-            color="white"
-            width="fit-content"
-            onClick={nextSlide}
-          />
-        </HStack>
-      </HStack>
+    <Section
+      title="Browse by Category"
+      heading="Categories"
+      rightSide={rightSide}
+    >
       <Box w="full">
         <SimpleSlider config={config} ref={sliderRef}>
           {categories.map((category) => (
-            <Box key={category}>
+            <Box key={category.name}>
               <Center
                 height={120}
                 margin={2}
                 border="1px solid"
                 borderRadius="lg"
                 padding={5}
+                flexDirection="column"
+                gap={2}
               >
-                {category}
+                <Image src={category.icon} boxSize={8} />
+                <Text fontWeight={600}>{category.name}</Text>
               </Center>
             </Box>
           ))}
         </SimpleSlider>
       </Box>
-    </Stack>
+    </Section>
   );
 };
+
+// <Box w="full">
+//   <SimpleSlider config={config} ref={sliderRef}>
+//     {categories.map((category) => (
+//       <Box key={category.name}>
+//         <Center
+//           height={120}
+//           margin={2}
+//           border="1px solid"
+//           borderRadius="lg"
+//           padding={5}
+//           flexDirection="column"
+//           gap={2}
+//         >
+//           <Image src={category.icon} boxSize={8} />
+//           <Text fontWeight={600}>{category.name}</Text>
+//         </Center>
+//       </Box>
+//     ))}
+//   </SimpleSlider>
+// </Box>;
 
 export default BrowseByCategory;
