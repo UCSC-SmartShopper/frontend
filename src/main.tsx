cbe9e75bf569edc0b8ec519c2720ea89d4f4869c
chathushkaayash <chathushkaayash@gmail.com>
2024-06-27 14:36:31 +0530

    refactor providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,41 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import App from "./App.tsx";
-import "./index.css";
-import theme from "./components/theme.ts";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Button } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 import { createTheme } from "@mui/material/styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import theme from "./components/theme.ts";
+import "./index.css";
 
 const muiTheme = createTheme();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={muiTheme}>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    {/* For Chakra UI */}
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+
+      {/* For Material UI */}
+      <ThemeProvider theme={muiTheme}>
         <QueryClientProvider client={new QueryClient()}>
           <App />
         </QueryClientProvider>
-      </ChakraProvider>
-    </ThemeProvider>
+      </ThemeProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-// import App from "./App";
-// import theme from './components/theme'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-//   <React.StrictMode>
-//     <ChakraProvider theme={theme}>
-//       <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-//       <App />
-//     </ChakraProvider>
-//   </React.StrictMode>
-// );
