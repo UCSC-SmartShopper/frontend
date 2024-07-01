806afe35d8bc8c8f45d5035fc5c83e853fcfc370
BimsaraAnjanaJay <bimsarajayadewa@gmail.com>
2024-07-01 23:45:02 +0530

    Add consumer profile page

--- src/pages/ConsumerProfile.tsx
+++ src/pages/ConsumerProfile.tsx
@@ -1,12 +1,24 @@
-import { Flex } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import ProfileDetail from "../components/Consumer/ProfileDetail";
 
 const ConsumerProfile = () => {
   return (
-    <Flex bg="background">
-        <ProfileDetail />
-    </Flex>
+    <>
+      <Grid
+        h="200px"
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(2, 1fr)"
+        gap={4}
+      >
+        <GridItem rowSpan={1} colSpan={1} bg="background">
+          <ProfileDetail />
+        </GridItem>
+        <GridItem rowSpan={2} bg="papayawhip" />
+
+        <GridItem colSpan={1} bg="papayawhip" />
+      </Grid>
+    </>
   );
-}
+};
 
 export default ConsumerProfile;
