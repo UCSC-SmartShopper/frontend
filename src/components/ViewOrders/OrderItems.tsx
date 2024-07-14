01f7871ec76944717ad642375fbc37baf0fcc477
BimsaraAnjanaJay <bimsarajayadewa@gmail.com>
2024-07-14 16:11:53 +0530

    Finalize order items component

--- src/components/ViewOrders/OrderItems.tsx
+++ src/components/ViewOrders/OrderItems.tsx
@@ -1,7 +1,26 @@
-import { Box, Text, Flex } from "@chakra-ui/react";
-import OrderItemRow from "./OrderItem";
+import {
+  Box,
+  Text,
+  Flex,
+  Image,
+  Button,
+  Modal,
+  useDisclosure,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/react";
+import AddProductReview from "./AddProductReview";
 
 const OrderItems = () => {
+  const {
+    isOpen: isAddReviewOpen,
+    onOpen: onAddReviewOpen,
+    onClose: onAddReviewClose,
+  } = useDisclosure();
+
   const items = [
     {
       image: "https://via.placeholder.com/50",
@@ -36,14 +55,54 @@ const OrderItems = () => {
         Order Items
       </Text>
       {items.map((item, index) => (
-        <OrderItemRow
+        <Flex
+          p={4}
           key={index}
-          image={item.image}
-          name={item.name}
-          price={item.price}
-          supermarket={item.supermarket}
-          supermarketLogo={item.supermarketLogo}
-        />
+          bg="background"
+          borderRadius={10}
+          alignItems="center"
+          justifyContent="space-between"
+          mb={4}
+        >
+          <Flex alignItems="center">
+            <Image
+              src={item.image}
+              alt={item.name}
+              boxSize="50px"
+              borderRadius="md"
+              mr={4}
+            />
+            <Box>
+              <Text fontWeight="bold">{item.name}</Text>
+              <Text color="gray.500">${item.price}</Text>
+            </Box>
+          </Flex>
+          <Flex alignItems="center" gap={4}>
+            <Image
+              src={item.supermarketLogo}
+              alt={item.supermarket}
+              boxSize="50px"
+            />
+            <Button
+              size="sm"
+              color="primary"
+              bg="white"
+              borderWidth={2}
+              borderColor="primary"
+              borderRadius={10}
+              onClick={onAddReviewOpen}
+              _hover={{ bg: "#E46C0A", color: "#FFFFFF" }}
+              _active={{
+                bg: "#E46C0A",
+                color: "#FFFFFF",
+                transform: "scale(0.98)",
+                borderColor: "#E46C0A",
+              }}
+            >
+              Add Reviews
+            </Button>
+          </Flex>
+        </Flex>
       ))}
       <Flex justifyContent="flex-end" mt={4}>
         <Text>
@@ -61,6 +120,44 @@ const OrderItems = () => {
           </Text>
         </Text>
       </Flex>
+      {/* Add Product Review Modal */}
+      <Modal
+        isOpen={isAddReviewOpen}
+        onClose={onAddReviewClose}
+        isCentered
+        closeOnOverlayClick={false}
+      >
+        <ModalOverlay backdropFilter="blur(5px)" />
+        <ModalContent borderRadius="15px">
+          <ModalHeader
+            fontSize="xl"
+            fontWeight="bold"
+            textAlign="center"
+            color="orange.500"
+          >
+            Add Product Review
+          </ModalHeader>
+          <ModalBody>
+            <AddProductReview productImage="https://via.placeholder.com/50" supermarketImage="https://via.placeholder.com/25" productName="Gradient Graphic T-shirt" price={45.6} />
+          </ModalBody>
+          <ModalFooter>
+            <Flex width="100%" justifyContent="center" columnGap={5} >
+              <Button
+                variant="outline"
+                colorScheme="orange"
+                px={5}
+                mr={3}
+                onClick={onAddReviewClose}
+              >
+                Cancel
+              </Button>
+              <Button colorScheme="orange" px={5} onClick={onAddReviewClose}>
+                Publish
+              </Button>
+            </Flex>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
