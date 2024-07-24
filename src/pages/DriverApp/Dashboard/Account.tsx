d07e53e38a2cc2247aa46fa048daa73490e8dff1
Nethmi Athukorala <nethmihao2001@gmail.com>
2024-07-24 17:07:05 +0530

    accountInfo page

--- src/pages/DriverApp/Dashboard/Account.tsx
+++ src/pages/DriverApp/Dashboard/Account.tsx
@@ -1,16 +1,17 @@
 // import useAuthStore from "@/state-management/auth/store";
 import {
-  Box,
+  Divider,
   HStack,
   Icon,
   Image,
+  Stack,
   Text,
   VStack,
 } from "@chakra-ui/react";
 import { BiSolidUserRectangle } from "react-icons/bi";
 import { PiCaretRightThin } from "react-icons/pi";
 import { FaCar } from "react-icons/fa";
-import { MdStarRate ,MdContactSupport } from "react-icons/md";
+import { MdStarRate, MdContactSupport } from "react-icons/md";
 import { TbTruckDelivery } from "react-icons/tb";
 import { useNavigate } from "react-router-dom";
 
@@ -19,15 +20,27 @@ const Account = () => {
   const navigate = useNavigate();
 
   const rows = [
-    { Icon:BiSolidUserRectangle ,label: "Account", path: "/driver/account/edit" },
-    { Icon:FaCar ,label: "Vehicle Details", path: "/driver/account/vehicle" },
-    { Icon:TbTruckDelivery ,label: "Deliveries", path: "/driver/account/deliveries" },
-    { Icon:MdStarRate ,label: "Ratings", path: "/driver/account/ratings" },
-    { Icon:MdContactSupport, label: "Support", path: "/driver/account/support" }
+    {
+      Icon: BiSolidUserRectangle,
+      label: "Account",
+      path: "/driver/account/edit",
+    },
+    { Icon: FaCar, label: "Vehicle Details", path: "/driver/account/vehicle" },
+    {
+      Icon: TbTruckDelivery,
+      label: "Deliveries",
+      path: "/driver/account/deliveries",
+    },
+    { Icon: MdStarRate, label: "Ratings", path: "/driver/account/ratings" },
+    {
+      Icon: MdContactSupport,
+      label: "Support",
+      path: "/driver/account/support",
+    },
   ];
   return (
     <>
-      <Box bg="white" borderWidth={1} borderRadius="lg" p={4}>
+      <Stack bg="white" borderWidth={2} p={5} gap="2vh">
         <Text fontSize="2xl" fontWeight="bold">
           Profile
         </Text>
@@ -49,17 +62,27 @@ const Account = () => {
             </Text>
           </VStack>
         </HStack>
-      </Box>
-      <Box p={4}>
+      </Stack>
+      <Stack p={5} divider={<Divider borderColor={"gray.400"} />}>
         {rows.map((row, index) => (
-        <HStack key={index} justify="space-between" px={2} py={4} onClick={()=>{navigate(row.path)}} cursor="pointer">
-          <HStack gap={5}>
-            <Icon as={row.Icon} color="primary" />
-            <Text>{row.label}</Text>
+          <HStack
+            key={index}
+            justify="space-between"
+            px={2}
+            py={4}
+            onClick={() => {
+              navigate(row.path);
+            }}
+            cursor="pointer"
+          >
+            <HStack gap={5}>
+              <Icon as={row.Icon} color="primary" boxSize={7} />
+              <Text>{row.label}</Text>
+            </HStack>
+            <Icon as={PiCaretRightThin} boxSize={5} />
           </HStack>
-          <Icon as={PiCaretRightThin} />
-        </HStack>))}
-      </Box>
+        ))}
+      </Stack>
       {/* <Heading>{user?.username}</Heading>
       <button onClick={logout}>Logout</button>
       <Box p={4}>
