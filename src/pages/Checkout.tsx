504d2da952027b29d0333394be5317a73fc64964
BimsaraAnjanaJay <bimsarajayadewa@gmail.com>
2024-07-24 16:41:45 +0530

    Add checkout page for consumer

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -12,74 +12,134 @@ import {
   Stack,
   Image,
   IconButton,
-} from '@chakra-ui/react';
-import { EditIcon, InfoIcon, CheckCircleIcon, ChevronRightIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import { EditIcon, InfoIcon } from "@chakra-ui/icons";
+import { IoIosArrowBack } from "react-icons/io";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+import { PiLightningFill } from "react-icons/pi";
 
 const Checkout = () => {
   return (
     <ChakraProvider>
       <Box maxWidth="1200px" margin="0 auto" padding="4">
         <Flex justifyContent="space-between" alignItems="center" mb={4}>
-          <Button variant="link">&lt; Back to store</Button>
+          <Button variant="link" size="xxl" color={"black"}>
+            <IoIosArrowBack />
+          </Button>
         </Flex>
 
-        <Flex flexDirection={['column', 'column', 'row']} gap={4}>
+        <Flex flexDirection={["column", "column", "row"]} gap={4}>
           {/* Left Column */}
           <Box flex="2">
             <VStack align="stretch" spacing={4}>
               {/* Delivery Details */}
               <Box border="1px" borderRadius="md" padding="4">
                 <HStack justify="space-between">
-                  <Heading size="md">Delivery details</Heading>
+                  <Heading size="md" color="orange.400">
+                    Delivery details
+                  </Heading>
                 </HStack>
                 <Divider my={2} />
                 <VStack align="stretch" spacing={2}>
                   <HStack justify="space-between">
                     <Box>
-                      <Text fontWeight="bold">Bandaragama Junction</Text>
-                      <Text>66 Pandura Rd, Bandaragama</Text>
+                      <HStack>
+                        <MdOutlineLocationOn />
+                        <VStack ml={3} alignItems={"unset"} spacing={0}>
+                          <Text fontWeight="bold">Bandaragama Junction</Text>
+                          <Text textAlign={"right"}>
+                            66 Pandura Rd, Bandaragama
+                          </Text>
+                        </VStack>
+                      </HStack>
                     </Box>
-                    <Button variant="link" size="sm" rightIcon={<EditIcon />}>Edit</Button>
+                    <Button
+                      size="sm"
+                      rightIcon={<EditIcon />}
+                      variant="outline"
+                      colorScheme="white"
+                      color="orange.400"
+                      borderColor="orange.400"
+                      border="1px"
+                      borderRadius="12px"
+                      fontSize="15px"
+                      fontWeight="bold"
+                      bg="white"
+                      _hover={{ bg: "orange.400", color: "white" }}
+                      _active={{
+                        bg: "orange.400",
+                        color: "white",
+                        transform: "scale(0.98)",
+                        borderColor: "orange.400",
+                      }}
+                    >
+                      Edit
+                    </Button>
                   </HStack>
                   <HStack justify="space-between">
                     <Box>
-                      <Text fontWeight="bold">Meet at my door</Text>
-                      <Text>Add delivery instructions</Text>
+                      <HStack>
+                        <FaRegUser />
+                        <VStack ml={3} alignItems={"unset"} spacing={0}>
+                          <Text fontWeight="bold">Meet at my door</Text>
+                          <Text>Add delivery instructions</Text>
+                        </VStack>
+                      </HStack>
                     </Box>
-                    <Button variant="link" size="sm" rightIcon={<EditIcon />}>Edit</Button>
+                    <Button
+                      variant="outline"
+                      colorScheme="white"
+                      color="orange.400"
+                      borderColor="orange.400"
+                      border="1px"
+                      borderRadius="12px"
+                      fontSize="15px"
+                      fontWeight="bold"
+                      bg="white"
+                      _hover={{ bg: "orange.400", color: "white" }}
+                      _active={{
+                        bg: "orange.400",
+                        color: "white",
+                        transform: "scale(0.98)",
+                        borderColor: "orange.400",
+                      }}
+                      size="sm"
+                      rightIcon={<EditIcon />}
+                    >
+                      Edit
+                    </Button>
                   </HStack>
                 </VStack>
               </Box>
 
               {/* Delivery Options */}
               <Box border="1px" borderRadius="md" padding="4">
-                <Heading size="md">Delivery options</Heading>
+                <Heading size="md" color="orange.400">
+                  Delivery options
+                </Heading>
                 <Divider my={2} />
                 <VStack align="stretch" spacing={2}>
-                  <Box border="1px" borderRadius="md" padding="4" borderColor="gray.200">
+                  <Box
+                    border="1px"
+                    borderRadius="md"
+                    padding="4"
+                    borderColor="gray.200"
+                  >
                     <HStack justify="space-between">
                       <Box>
-                        <Text fontWeight="bold">Priority</Text>
-                        <Badge colorScheme="green">Faster</Badge>
-                        <Text>10-25 min • Delivered directly to you</Text>
+                        <HStack >
+                          <PiLightningFill color="green"/>
+                          <VStack ml={3} alignItems={"unset"} spacing={0}>
+                            <HStack spacing={4}>
+                              <Text fontWeight="bold">Priority</Text>
+                              <Badge colorScheme="green" borderRadius={24}>Faster</Badge>
+                            </HStack>
+                            <Text>10-25 min • Delivered directly to you</Text>
+                          </VStack>
+                        </HStack>
                       </Box>
                       <Text fontWeight="bold">+LKR 129.00</Text>
-                    </HStack>
-                  </Box>
-                  <Box border="1px" borderRadius="md" padding="4" borderColor="black">
-                    <HStack justify="space-between">
-                      <Box>
-                        <Text fontWeight="bold">Standard</Text>
-                        <Text>15-30 min</Text>
-                      </Box>
-                    </HStack>
-                  </Box>
-                  <Box border="1px" borderRadius="md" padding="4" borderColor="gray.200">
-                    <HStack justify="space-between">
-                      <Box>
-                        <Text fontWeight="bold">Schedule</Text>
-                        <Text>Select a time</Text>
-                      </Box>
                     </HStack>
                   </Box>
                 </VStack>
@@ -87,16 +147,39 @@ const Checkout = () => {
 
               {/* Payment */}
               <Box border="1px" borderRadius="md" padding="4">
-                <Heading size="md">Payment</Heading>
+                <Heading size="md" color="orange.400">
+                  Payment
+                </Heading>
                 <Divider my={2} />
                 <HStack justify="space-between">
                   <Text>Add payment method</Text>
-                  <Button variant="link" size="sm" rightIcon={<EditIcon />}>Edit</Button>
+                  <Button
+                    variant="outline"
+                    colorScheme="white"
+                    color="orange.400"
+                    borderColor="orange.400"
+                    border="1px"
+                    borderRadius="12px"
+                    fontSize="15px"
+                    fontWeight="bold"
+                    bg="white"
+                    _hover={{ bg: "orange.400", color: "white" }}
+                    _active={{
+                      bg: "orange.400",
+                      color: "white",
+                      transform: "scale(0.98)",
+                      borderColor: "orange.400",
+                    }}
+                    size="sm"
+                    rightIcon={<EditIcon />}
+                  >
+                    Edit
+                  </Button>
                 </HStack>
               </Box>
 
               {/* Continue to payment */}
-              <Button colorScheme="blackAlpha" size="lg" width="full">
+              <Button bg="black" size="lg" width="full" color="white">
                 Continue to payment
               </Button>
             </VStack>
@@ -108,39 +191,65 @@ const Checkout = () => {
               {/* Order Summary */}
               <Box border="1px" borderRadius="md" padding="4">
                 <HStack justify="space-between">
-                  <Heading size="md">Pizza Hut</Heading>
+                  <Image
+                    borderRadius="50%"
+                    src="https://via.placeholder.com/60"
+                    alt="Order Item"
+                  />
+                  <VStack>
+                    <Heading size="md" color="orange.400">
+                      Arpico Supermarket
+                    </Heading>
+                    <Text>Mirisswaththa, Piliyandala</Text>
+                  </VStack>
                   <IconButton
                     aria-label="Restaurant Info"
                     icon={<InfoIcon />}
-                    size="sm"
-                    variant="ghost"
+                    size="md"
+                    variant={"outline"}
+                    colorScheme="white"
+                    border="1px"
+                    borderRadius="12px"
+                    fontSize="15px"
+                    fontWeight="bold"
+                    bg="white"
+                    _hover={{ bg: "orange.400", color: "white" }}
+                    _active={{
+                      bg: "orange.400",
+                      color: "white",
+                      transform: "scale(0.98)",
+                      borderColor: "orange.400",
+                    }}
                   />
                 </HStack>
-                <Text>66 Panadura Rd, Panadura</Text>
+
                 <Box my={2}>
-                  <Image
-                    borderRadius="md"
-                    src="https://via.placeholder.com/150"
-                    alt="Order Item"
-                  />
-                  <Text mt={2}>Save LKR 138.00 on this order with Uber One</Text>
-                  <Button size="sm" mt={2}>Get yours now</Button>
+                  <HStack gridTemplateColumns={"40% 60%"}>
+                    <VStack>
+                      <Text mt={2}>
+                        Save LKR 138.00 on this order with Uber One
+                      </Text>
+                      <Button size="sm" mt={2}>
+                        Get yours now
+                      </Button>
+                    </VStack>
+                    <Image
+                      borderRadius="md"
+                      src="https://via.placeholder.com/120"
+                      alt="Order Item"
+                    />
+                  </HStack>
                 </Box>
-                <Button colorScheme="blackAlpha" size="lg" width="full">
+                <Button bg="black" size="lg" width="full" color="white">
                   Continue to payment
                 </Button>
               </Box>
 
-              {/* Promotion */}
-              <Box border="1px" borderRadius="md" padding="4">
-                <Heading size="md">Promotion</Heading>
-                <Divider my={2} />
-                <Button variant="link">Add promo code</Button>
-              </Box>
-
               {/* Order Total */}
               <Box border="1px" borderRadius="md" padding="4">
-                <Heading size="md">Order total</Heading>
+                <Heading size="md" color="orange.400">
+                  Order total
+                </Heading>
                 <Divider my={2} />
                 <Stack spacing={1}>
                   <HStack justify="space-between">
@@ -165,7 +274,9 @@ const Checkout = () => {
 
               {/* Note */}
               <Text fontSize="sm">
-                If you're not around when the delivery person arrives, they'll leave your order at the door. By placing your order, you agree to take full responsibility for it once it's delivered. Orders containing alcohol or other restricted items may not be eligible for leave at door and will be returned to the store if you are not available.
+                If you're not around when the delivery person arrives, If you're
+                not around when the delivery person arrives, If you're not
+                around when the delivery person arrives
               </Text>
             </VStack>
           </Box>
