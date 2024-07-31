0d35bfa46dbd559815453bc0d1cc4fef79ab48a1
milindaShehan <milinda.msd@gmail.com>
2024-08-01 01:05:34 +0530

    product ui update

--- src/pages/SupermarketManager/Products.tsx
+++ src/pages/SupermarketManager/Products.tsx
@@ -7,7 +7,8 @@ const ProductDetails = () => {
         <>
             <Box p={3} bg={"white"} borderRadius={20} overflow="hidden"
         boxShadow="md"
-        borderWidth={2}>
+        borderWidth={2}
+        my={10}>
                 <Text fontSize="2xl" fontWeight="semibold" mb={4}>
                     Product Details 
                 </Text>
