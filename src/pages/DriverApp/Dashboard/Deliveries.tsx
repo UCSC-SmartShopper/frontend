c304fa281a36eb454642893ecde3af040cb7e663
Nethmi Athukorala <nethmihao2001@gmail.com>
2024-08-19 14:36:25 +0530

    deliveries

--- src/pages/DriverApp/Dashboard/Deliveries.tsx
+++ src/pages/DriverApp/Dashboard/Deliveries.tsx
@@ -1,44 +1,55 @@
+import useOpportunities from "@/hooks/useOpportunities";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { Box, HStack, IconButton, Text, VStack } from "@chakra-ui/react";
 import { useState } from "react";
 
 const Deliveries = () => {
+  const opportunities = useOpportunities("Delivered");
   const [isRotated, setIsRotated] = useState(false);
   const displayDetails = () => {
     setIsRotated(!isRotated);
   };
-
+  // console.log(opportunities.data?.results);
   return (
     <VStack spacing={4} mt={4} w="full" justifyContent="center">
-      <Box bg="white" borderRadius="lg" shadow="md" p={4} w="90vw">
-        <HStack>
-          <VStack align="start" flex="1">
-            <Text fontWeight="bold">Warrington, PA 76102</Text>
-            <Text color="gray.500" fontSize="sm">
-              Yesterday at 16:34
+      {opportunities.data?.results.map((opportunity, index) => (
+        <Box
+          bg="white"
+          borderRadius="lg"
+          shadow="md"
+          p={4}
+          w="90vw"
+          key={index}
+        >
+          <HStack>
+            <VStack align="start" flex="1">
+              <Text fontWeight="bold">{opportunity.deliveryLocation}</Text>
+              <Text color="gray.500" fontSize="sm">
+                Yesterday at 16:34
+              </Text>
+            </VStack>
+            <Text fontWeight="bold" color="red.500">
+              Rs 100
             </Text>
-          </VStack>
-          <Text fontWeight="bold" color="red.500">
-            Rs 100
-          </Text>
-          <IconButton
-            onClick={displayDetails}
-            aria-label="Go to delivery"
-            icon={<ChevronRightIcon />}
-            variant="ghost"
-            transform={isRotated ? "rotate(90deg)" : "rotate(0deg)"}
-          />
-        </HStack>
+            <IconButton
+              onClick={displayDetails}
+              aria-label="Go to delivery"
+              icon={<ChevronRightIcon />}
+              variant="ghost"
+              transform={isRotated ? "rotate(90deg)" : "rotate(0deg)"}
+            />
+          </HStack>
 
-        {isRotated && (
-          <Box>
-            <HStack justify="space-between">
-              <Text>Delivery cost</Text>
-              <Text>100</Text>
-            </HStack>
-          </Box>
-        )}
-      </Box>
+          {isRotated && (
+            <Box>
+              <HStack justify="space-between">
+                <Text>Delivery cost</Text>
+                <Text>100</Text>
+              </HStack>
+            </Box>
+          )}
+        </Box>
+      ))}
     </VStack>
   );
 };
