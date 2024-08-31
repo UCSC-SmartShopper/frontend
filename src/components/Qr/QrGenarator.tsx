fbedb36414a57d0f0daafa0c257415348d1e1ce5
chathushkaayash <chathushkaayash@gmail.com>
2024-08-31 12:55:09 +0530

    refactor: Update file paths and remove unused imports

--- src/components/Qr/QrGenarator.tsx
+++ src/components/Qr/QrGenarator.tsx
@@ -1,12 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import QRCode from "react-qr-code";
 
-
 export default function QrGenarator() {
-    return (
-        <div>
-        <QRCode value="hey machan" />
-        </div>
-    );
-    }
+  return (
+    <div>
+      <QRCode value="hey machan" />
+    </div>
+  );
+}
