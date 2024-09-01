50c065e216e99866e7f8a4d7e123cef63c71f406
milindaShehan <milinda.msd@gmail.com>
2024-09-01 18:21:15 +0530

    supermarket manager form

--- src/Routes/AdminRoutes.tsx
+++ src/Routes/AdminRoutes.tsx
@@ -8,6 +8,7 @@ const AdminOverview = loadable(() => import("@/pages/Admin/AdminOverview"));
 const AdminSettings = loadable(() => import("@/pages/Admin/AdminSettings"));
 const AdminSuperMarkets = loadable(() => import("@/pages/Admin/AdminSuperMarkets"));
 const AdminTransactions = loadable(() => import("@/pages/Admin/AdminTransactions"));
+const AdminSupermarketForm = loadable(() => import("@/pages/Admin/Forms/SupermarketForm"));
 
 import AdminLayout from "./Layouts/AdminLayout";
 import Supermarkets from "@/pages/Admin/Supermarkets";
@@ -27,7 +28,8 @@ const AdminRoutes = [
       { path: "transactions", element: <AdminTransactions /> },
       { path: "advertisements", element: <AdminAdvertisements /> },
       { path: "settings", element: <AdminSettings /> },
-      {path: "supermarketList", element: <Supermarkets />}
+      {path: "supermarketList", element: <Supermarkets />},
+      {path: "addSupermarket", element: <AdminSupermarketForm />}
 
     ],
   },
