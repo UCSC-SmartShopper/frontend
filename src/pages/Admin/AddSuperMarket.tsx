ba5b4d891c10efb469f9b6231d4039c6a3dbd616
Kaveesha Hettige <2021is046@stu.ucsc.cmb.ac.lk>
2024-12-03 17:59:27 +0530

    feat: enhance AddSuperMarket page with improved layout and heading

--- src/pages/Admin/AddSuperMarket.tsx
+++ src/pages/Admin/AddSuperMarket.tsx
@@ -1,9 +1,28 @@
+import { Box, Center, Heading, HStack, Icon } from "@chakra-ui/react"
+import SupermarketForm from "./Forms/SupermarketForm"
+import { GiShop } from "react-icons/gi";
 
 
 const AddSuperMarket = () => {
   return (
    <>
-   add supermarket
+   <Box my={10} px={10} >
+   <HStack align="center" spacing={4} mb={6}> {/* align items centrally and add spacing between */}
+  <Icon
+    as={GiShop}
+    w={10}
+    h={10}
+    color="primary"
+  />
+  <Center> {/* Ensures that the heading is centered */}
+    <Heading size="lg"> {/* Adjusts the bottom margin */}
+      Add New SuperMarket
+    </Heading>
+  </Center>
+</HStack>
+
+    <SupermarketForm/>
+   </Box>
    </>
   )
 }
